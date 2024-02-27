37f869051d9ca7b99ddf5e33c70ee32c09c22b05
sachin <contact2sachin11@gmail.com>
2024-02-27 16:49:45 +0530

    added images from json file

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -3,17 +3,33 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as productsJson from './../../../products.json';
 import * as categoryJson from './../../../categories.json';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent implements OnInit {
   json: any;
   category_json: any;
+
+  productObj: any = {
+    productId: 690,
+    productSku: 'Winkies',
+    productName: 'Winkies Layer Cake - Chocolate',
+    productPrice: 5.0,
+    productShortName: 'Winkies Layer Cake - Chocolate',
+    productDescription: 'Winkies Layer Cake - Chocolate, 15 g',
+    createdDate: new Date(),
+    deliveryTimeSpan: '1day',
+    categoryId: 69,
+    productImageUrl:
+      'https://www.bigbasket.com/media/uploads/p/l/40018011_4-winkies-layer-cake-chocolate.jpg',
+    categoryName: 'Cakes & Pastries',
+  };
   ngOnInit(): void {
     this.json = productsJson;
     this.category_json = categoryJson;
