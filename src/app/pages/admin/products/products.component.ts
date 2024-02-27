dbb0fbc918d52b5e39a38a92864490f61fe4ea7c
sachin <contact2sachin11@gmail.com>
2024-02-27 15:57:52 +0530

    improvments

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as productsJson from './../../../products.json';
+import * as categoryJson from './../../../categories.json';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+  json: any;
+  category_json: any;
+  ngOnInit(): void {
+    this.json = productsJson;
+    this.category_json = categoryJson;
+    // console.log('Data', this.json.data);
+    console.log('Data', this.category_json.data);
+  }
+  isProductPanelVisible = false;
+
+  onCloseClick() {
+    this.isProductPanelVisible = false;
+  }
+  onNewClick() {
+    this.isProductPanelVisible = true;
+  }
+}
