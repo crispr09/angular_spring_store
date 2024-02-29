20e3bdda8eab667b87a6c59c5ffa3d77c8d3e66e
sachin <contact2sachin11@gmail.com>
2024-02-29 14:15:13 +0530

    improvments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,22 @@ import { LoginComponent } from './pages/admin/login/login.component';
 import { LayoutComponent } from './pages/admin/layout/layout.component';
 import { ProductsComponent } from './pages/admin/products/products.component';
 import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'shop',
     pathMatch: 'full',
   },
   {
     path: 'login',
     component: LoginComponent,
   },
-
+  {
+    path: 'shop',
+    component: LandingComponent,
+  },
   {
     path: '',
     component: LayoutComponent,
